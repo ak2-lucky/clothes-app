788a1c9b09797a5286ab8268a8cb0b8d888a707c
EC2 Default User <ak2monotoe@gmail.com>
2020-04-02 11:58:32 +0000

    viewの調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'staticpages#home'
   get  '/about',             to: 'staticpages#about'
-  resources :posts,          only: [:new, :index, :show, :create] do
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users,          only: [:show]
+  resources :posts,          only: [:new, :index, :show, :create, :destroy] do
     resources :comments,     only: [:create, :destroy]
   end
-  resources :posts,          only: [:destroy]
-  resources :users,          only: [:show]
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
