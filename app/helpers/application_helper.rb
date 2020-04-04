df3883b4b2fc319a76cfe3a3e3e41374b6102bb1
EC2 Default User <ak2monotoe@gmail.com>
2020-04-04 04:24:55 +0000

    helperにheader用のメソッド追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,11 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+  
+  
+  def active_page(path_name)
+    if  current_page?(path_name)
+      return "active"
+    end
+  end
 end
